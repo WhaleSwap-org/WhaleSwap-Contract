e62bc791a3f6317be2d452b23092ca8b70054779
chrypnotoad <cafreels@gmail.com>
2026-02-09 19:09:27 -0600

    Add Polygon verify args

---
+++ scripts/verify-args-polygon.js
@@ -0,0 +1,13 @@
+// Constructor args for Polygon deployment at 0x324d9b90A07D587B4FA0D68c22645B9c8D321079
+module.exports = [
+  // feeToken (Polygon USDC)
+  "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+  // feeAmount (1 USDC @ 6 decimals)
+  "1000000",
+  // allowedTokens
+  [
+    "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+    "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6"
+  ]
+];
+
